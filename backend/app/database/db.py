49935b43ad42671432223334fe2c6715d891b93d
Dznzx <dineshkarthickl158@gmail.com>
2026-05-25 17:46:14 +0530

    fix

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -5,10 +5,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 
 SessionLocal = sessionmaker(
