8b6a318b2bc427486c05c8d81d9b8afe61e06f8f
Dznzx <dineshkarthickl158@gmail.com>
2026-05-25 17:40:33 +0530

    Fixed Render Neon database connection

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -2,14 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://localhost/studentdb"
+import os
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
